a7a38940cb27a91fa00e5478f7389c14590778c7
Alejandro Tabares Bosquet <alejandro.tabares1@tesco.com>
2020-08-21 08:51:22 +0100

    Logging request path

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  console.log(`Received request in: ${req.path}`);
+  res.send("Hello World!");
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
